c4f834fbe8c41b18b0db2fe7da6f784954010a54
pesta <andru.pestov@gmai.com>
2022-04-22 23:47:41 +0300

    now linear trajectory via norm fucn working

--- VKR/workspace/src/velodyne_cartographer/champ_trajectory_1.py
+++ VKR/workspace/src/velodyne_cartographer/champ_trajectory_1.py
@@ -2,6 +2,7 @@
 #credits to: https://github.com/ros-teleop/teleop_twist_keyboard/blob/master/teleop_twist_keyboard.py
 
 from __future__ import print_function
+from math import sin, cos, pi
 
 import roslib; roslib.load_manifest('champ_teleop')
 import rospy
@@ -35,80 +36,96 @@ def turn_back(odom_now, start_turn_pose):
   else:
     return False
 
+def y_from_quaternion(q):
+  orientation_q = q.pose.pose.orientation
+  orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+  (r, p, y) = euler_from_quaternion (orientation_list)
+  return y
 
-
-
-def main():
-  velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)    
-  joy_subscriber = rospy.Subscriber('odom/ground_truth', Odometry, odometry_callback)
-  twist = Twist()
-  r = rospy.Rate(10) # 10hz 
-  r.sleep()
-  now = rospy.get_rostime()
+def go_forwart(distance):
   start_odom = odom
-  start_turn_pose = odom
-  stage = 0
-  while not rospy.is_shutdown():
+  r = rospy.Rate(10)
+  
+  print(y_from_quaternion(odom), odom.pose.pose.position.x,start_odom.pose.pose.position.x, distance*cos(y_from_quaternion(odom)), )
+  if (odom.pose.pose.position.x < start_odom.pose.pose.position.x + (distance*cos(y_from_quaternion(odom)))):
+    while (odom.pose.pose.position.x < start_odom.pose.pose.position.x + (distance*cos(y_from_quaternion(odom)))):
+      twist.linear.x = 0.4
+      twist.linear.y = 0.0
+      twist.linear.z = 0.0
+      twist.angular.x = 0.0
+      twist.angular.y = 0.0
+      twist.angular.z = 0.0
+      velocity_publisher.publish(twist)
+      print(odom.pose.pose.position.x)
+      r.sleep()
+    return
+  if (odom.pose.pose.position.x > start_odom.pose.pose.position.x + (distance*cos(y_from_quaternion(odom)))):
+    while (odom.pose.pose.position.x > start_odom.pose.pose.position.x + (distance*cos(y_from_quaternion(odom)))):
+      twist.linear.x = 0.4
+      twist.linear.y = 0.0
+      twist.linear.z = 0.0
+      twist.angular.x = 0.0
+      twist.angular.y = 0.0
+      twist.angular.z = 0.0
+      velocity_publisher.publish(twist)
+      print(odom.pose.pose.position.x)
+      r.sleep()
+    return
+
+def turn_left(angle):
+  start_odom = odom
+  r = rospy.Rate(10)
+  while (y_from_quaternion(odom) < ((y_from_quaternion(start_odom) + angle) % (pi))):
+    print(y_from_quaternion(odom), y_from_quaternion(start_odom), angle)
     twist.linear.x = 0.0
     twist.linear.y = 0.0
     twist.linear.z = 0.0
     twist.angular.x = 0.0
     twist.angular.y = 0.0
-    twist.angular.z = 0.0
-    
-    print (now.secs, rospy.Time.now().secs)
-    
-    if ((now.secs < rospy.Time.now().secs - 1) and (stage < 2)):
-      stage = 1
-     
-      twist.linear.x = 0.0
-      twist.linear.y = 0.0
-      twist.linear.z = 0.0
-      twist.angular.x = 0.0
-      twist.angular.y = 0.0
-      twist.angular.z = -0.1
-
-    if (now.secs < rospy.Time.now().secs - 2) and stage < 3:
-      stage = 2
-      twist.linear.x = 0.4
-      twist.linear.y = 0.0
-      twist.linear.z = 0.0
-      twist.angular.x = 0.0
-      twist.angular.y = 0.0
-      twist.angular.z = 0.0
-      start_turn_pose = odom
-    if (odom.pose.pose.position.x > start_odom.pose.pose.position.x + 7) and stage < 4:
-      stage = 3
-      twist.linear.x = 0.0
-      twist.linear.y = 0.0
-      twist.linear.z = 0.0
-      twist.angular.x = 0.0
-      twist.angular.y = 0.0
-      twist.angular.z = 0.3
-      
-    if (turn_back(odom, start_turn_pose)) and stage < 5:
-      stage = 4
-      
-      
-      twist.linear.x = 0.4
-      twist.linear.y = 0.0
-      twist.linear.z = 0.0
-      twist.angular.x = 0.0
-      twist.angular.y = 0.0
-      twist.angular.z = 0.0
-    if (odom.pose.pose.position.x < start_odom.pose.pose.position.x) and stage > 3:
-
-      start_turn_pose = odom
-      twist.linear.x = 0.0
-      twist.linear.y = 0.0
-      twist.linear.z = 0.0
-      twist.angular.x = 0.0
-      twist.angular.y = 0.0
-      twist.angular.z = 0.0
+    twist.angular.z = 0.4
     velocity_publisher.publish(twist)
+    
+    r.sleep()
+  
+
+def turn_right(angle):
+  start_odom = odom
+  r = rospy.Rate(10)
+  
+  while (y_from_quaternion(odom) > ((y_from_quaternion(start_odom) - angle) % (pi))):
+
+    print(y_from_quaternion(odom), y_from_quaternion(start_odom), angle)
+    twist.linear.x = 0.0
+    twist.linear.y = 0.0
+    twist.linear.z = 0.0
+    twist.angular.x = 0.0
+    twist.angular.y = 0.0
+    twist.angular.z = -0.4
+    velocity_publisher.publish(twist)
+    
     r.sleep()
 
-   
+
+
+
+def main():
+  global velocity_publisher
+  velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)    
+  joy_subscriber = rospy.Subscriber('odom/ground_truth', Odometry, odometry_callback)
+  global twist
+  twist = Twist()
+  r = rospy.Rate(10)
+  r.sleep()
+  turn_right(0.1)
+
+  go_forwart(7)
+  turn_left(3.1415)
+  go_forwart(7)
+
+
+
+
+
 
 if __name__ == "__main__":
     rospy.init_node('champ_teleop')
