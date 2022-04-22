7b81fe06c7bdc912ee378351a544e1f3c30682f3
pesta <andru.pestov@gmai.com>
2022-04-22 22:57:05 +0300

    working linear trajectory

--- VKR/workspace/src/velodyne_cartographer/champ_trajectory_1.py
+++ VKR/workspace/src/velodyne_cartographer/champ_trajectory_1.py
@@ -7,6 +7,7 @@ import roslib; roslib.load_manifest('champ_teleop')
 import rospy
 
 from sensor_msgs.msg import Joy
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from champ_msgs.msg import Pose as PoseLite
 from geometry_msgs.msg import Pose as Pose
@@ -14,15 +15,39 @@ import tf
 
 import sys, select, termios, tty
 import numpy as np
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+
+def odometry_callback(data):
+  global odom
+  odom = data
+
+def turn_back(odom_now, start_turn_pose):
+  orientation_q = odom_now.pose.pose.orientation
+  orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+  (roll_now, pitch_now, yaw_now) = euler_from_quaternion (orientation_list)
+  orientation_s = start_turn_pose.pose.pose.orientation
+  orientation_list = [orientation_s.x, orientation_s.y, orientation_s.z, orientation_s.w]
+  (roll_start, pitch_start, yaw_start) = euler_from_quaternion (orientation_list)
+  
+  if (yaw_now > yaw_start + 3.1415):
+    return True
+  else:
+    return False
+
+
 
 
 def main():
   velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)    
+  joy_subscriber = rospy.Subscriber('odom/ground_truth', Odometry, odometry_callback)
   twist = Twist()
   r = rospy.Rate(10) # 10hz 
   r.sleep()
   now = rospy.get_rostime()
-
+  start_odom = odom
+  start_turn_pose = odom
+  stage = 0
   while not rospy.is_shutdown():
     twist.linear.x = 0.0
     twist.linear.y = 0.0
@@ -32,7 +57,9 @@ def main():
     twist.angular.z = 0.0
     
     print (now.secs, rospy.Time.now().secs)
-    if (now.secs < rospy.Time.now().secs - 1):
+    
+    if ((now.secs < rospy.Time.now().secs - 1) and (stage < 2)):
+      stage = 1
      
       twist.linear.x = 0.0
       twist.linear.y = 0.0
@@ -41,24 +68,27 @@ def main():
       twist.angular.y = 0.0
       twist.angular.z = -0.1
 
-    if (now.secs < rospy.Time.now().secs - 2):
-      
+    if (now.secs < rospy.Time.now().secs - 2) and stage < 3:
+      stage = 2
       twist.linear.x = 0.4
       twist.linear.y = 0.0
       twist.linear.z = 0.0
       twist.angular.x = 0.0
       twist.angular.y = 0.0
       twist.angular.z = 0.0
-    if (now.secs < rospy.Time.now().secs - 15):
-      
+      start_turn_pose = odom
+    if (odom.pose.pose.position.x > start_odom.pose.pose.position.x + 7) and stage < 4:
+      stage = 3
       twist.linear.x = 0.0
       twist.linear.y = 0.0
       twist.linear.z = 0.0
       twist.angular.x = 0.0
       twist.angular.y = 0.0
       twist.angular.z = 0.3
-    if (now.secs < rospy.Time.now().secs - 22):
-
+      
+    if (turn_back(odom, start_turn_pose)) and stage < 5:
+      stage = 4
+      
       
       twist.linear.x = 0.4
       twist.linear.y = 0.0
@@ -66,9 +96,9 @@ def main():
       twist.angular.x = 0.0
       twist.angular.y = 0.0
       twist.angular.z = 0.0
-    if (now.secs < rospy.Time.now().secs - 40):
+    if (odom.pose.pose.position.x < start_odom.pose.pose.position.x) and stage > 3:
 
-      
+      start_turn_pose = odom
       twist.linear.x = 0.0
       twist.linear.y = 0.0
       twist.linear.z = 0.0
